27d816041a709e5b16b6f3e392cda6e9a793b04d
PKSMG2 <pranav7471@outlook.com>
2025-08-03 22:57:52 +1200

    fix program theme not being loaded

--- MathsGame_3.0.py
+++ MathsGame_3.0.py
@@ -10,7 +10,7 @@ global timer_state , qnum , score_num
 timer_state = 1 # set the timers state to 1 which means its active
 score_num = 0
 ctk.set_appearance_mode("light")
-ctk.set_default_color_theme('Maths-Quiz-3PAD\program_theme.json')
+ctk.set_default_color_theme('program_theme.json')
 #Home Window
 class Home(ctk.CTk): #Initialise a class for the main window.
     def __init__(self):
